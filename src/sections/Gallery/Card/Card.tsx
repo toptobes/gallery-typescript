210c9e36b87468ac64167ed691543f0764e44ba7
toptobes <kavinpg@gmail.com>
2024-01-31 09:17:25 -0600

    made 404 image preload

--- src/sections/Gallery/Card/Card.tsx
+++ src/sections/Gallery/Card/Card.tsx
@@ -6,6 +6,7 @@ import { Similar } from '~/sections/Gallery/Card/Similar.tsx';
 import clsx from 'clsx';
 import { FoundIn } from '~/sections/Gallery/Card/FoundIn.tsx';
 import { UseFilter } from '~/lib/filter.ts';
+import p404 from '~/assets/images/404.webp';
 
 export const Card = ({ id, title, url, tags, difficulty, time, yt, gh, searchField, filter, filterDispatch }: AppInfo & UseFilter) => {
   const ref = useRef<HTMLDivElement>(null);
@@ -24,12 +25,14 @@ export const Card = ({ id, title, url, tags, difficulty, time, yt, gh, searchFie
     return () => observer.disconnect();
   }, []);
 
+  const src = (url && inView) ? url : p404
+
   return <article
     aria-label="App card"
-    className={clsx(inView ? s.cardImg : s.cardImgLazy, searchField && s.marginTop)}
-    style={{ '--bg-url': `url("${url}")` } as CSSProperties}
+    className={clsx(s.card, searchField && s.marginTop)}
     ref={ref}
   >
+    <img src={src} alt="" className={s.background} rel={src === p404 ? 'preload' : undefined}/>
     <div className={s.overlay}>
       <Header title={title} tags={tags} filterDispatch={filterDispatch} filter={filter}/>
     </div>
