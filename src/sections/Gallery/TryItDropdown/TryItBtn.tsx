9c20b618dfec661b88b09f24527e41157e45f237
toptobes <kavinpg@gmail.com>
2024-02-12 13:26:00 -0600

    Fixed 'Try It Now' button not working on certain browsers

--- src/sections/Gallery/TryItDropdown/TryItBtn.tsx
+++ src/sections/Gallery/TryItDropdown/TryItBtn.tsx
@@ -1,16 +1,31 @@
 import s from './TryItBtn.module.scss';
 import { AppLinks } from '~/lib/api/apps.ts';
 import { TryItDropdown } from '~/sections/Gallery/TryItDropdown/TryItDropdown.tsx';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   links: AppLinks
 }
 
 export const TryItBtn = ({ links }: Props) => {
+  const btnRef = useRef<HTMLButtonElement>(null);
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  return <button className={s.tryItNow} onFocus={() => setDropdownOpen(true)} onBlur={(e) => !e.currentTarget.contains(e.relatedTarget) && setDropdownOpen(false)}>
+  const openDropdown = () => setDropdownOpen(true);
+
+  useEffect(() => {
+    const closeDropdown = (e: MouseEvent) => {
+      if (btnRef.current && !btnRef.current.contains(e.target as Node)) {
+        setDropdownOpen(false);
+      }
+    }
+
+    document.body.addEventListener('click', closeDropdown);
+    return () => document.body.removeEventListener('click', closeDropdown);
+  }, []);
+
+  return <button className={s.tryItNow} onClick={openDropdown} ref={btnRef}>
     {dropdownOpen && <TryItDropdown links={links}/>}
     <span>Try It Now</span>
   </button>;
